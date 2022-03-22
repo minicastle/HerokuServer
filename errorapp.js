e3cb6c50dc05fe3d499114a2e4864a8f46028c08
minicastle <jk598200167@gmail.com>
2022-03-22 16:40:55 +0900

    exchange 20220322

--- errorapp.js
+++ errorapp.js
@@ -1,35 +1,13 @@
+const { default: axios } = require("axios");
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
-const axios = require("axios");
-const urlCrawl = require("./dataCrawl.json");
+const PORT = process.env.PORT||5000;
+const data = require("./dataCrawl.json");
 
 app.get("/",function(req,res){
-    res.send("Server Set start with Heroku");
+    res.send("Server Set Start with Heroku");
 });
-app.get("/nowdate",function(req,res){
-    let now = new Date;
-    let year = String(now.getFullYear());
-    let month = function(){
-        let buf = String(now.getMonth()+1);
-        if(buf.length!==2){
-            return "0"+buf;
-        }
-        else{
-            return buf;
-        }
-    };
-    let date =  function(){
-        let buf = String(now.getDate());
-        if(buf.length!==2){
-            return "0"+buf;
-        }
-        else{
-            return buf;
-        }
-    }
-    res.send(year+month()+date());
-});
+
 app.get("/exchange_rate/nowdate",function(req,res){
     let nowDate = function(){
         let now = new Date;
@@ -55,7 +33,7 @@ app.get("/exchange_rate/nowdate",function(req,res){
         return (year+month()+date());
     }
     console.log(nowDate());
-    axios.request(urlCrawl.url1+urlCrawl.authkey+urlCrawl.url2+nowDate()+urlCrawl.url3).then(function(data){
+    axios.request(data.url1+data.authkey+data.url2+nowDate()+data.url3).then(function(data){
         console.log(data);
         for(let i = 0;i<data.data.length;i++){
             delete data.data[i].result;
@@ -64,6 +42,30 @@ app.get("/exchange_rate/nowdate",function(req,res){
         }
         console.log(data.data);
         res.send(data.data);
-    });
+    })
 });
-app.listen(port);
+
+app.get("/nowdate",function(req,res){
+    let now = new Date;
+    let year = String(now.getFullYear());
+    let month = function(){
+        let buf = String(now.getMonth()+1);
+        if(buf.length!==2){
+            return "0"+buf;
+        }
+        else{
+            return buf;
+        }
+    };
+    let date =  function(){
+        let buf = String(now.getDate());
+        if(buf.length!==2){
+            return "0"+buf;
+        }
+        else{
+            return buf;
+        }
+    }
+    res.send(year+month()+date());
+})
+app.listen(PORT);
