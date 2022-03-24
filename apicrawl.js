987a89da52c7dca7af6444e112559f87a5c8d1b0
minicastle <jk598200167@gmail.com>
2022-03-24 10:56:50 +0900

    exchange 20220324

---
+++ apicrawl.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const datacrawl = require("./datacrawl.json");
+
+function nowDate(){
+    let now = new Date;
+    let year = String(now.getFullYear());
+    let month = function(){
+        let buf = String(now.getMonth()+1);
+        if(buf.length!==2){
+            return "0"+buf;
+        }
+        else{
+            return buf;
+        }
+    };
+    let date =  function(){
+        let buf = String(now.getDate());
+        if(buf.length!==2){
+            return "0"+buf;
+        }
+        else{
+            return buf;
+        }
+    }
+    return (year+month()+date());
+}
+
+async function apiCrawl(){
+    let value = await axios.get(datacrawl.url1+datacrawl.authkey+datacrawl.url2+nowDate()+datacrawl.url3).then(function(data){
+        for(let i = 0;i<data.data.length;i++){
+            delete data.data[i].result;
+            delete data.data[i].yy_efee_r;
+            delete data.data[i].ten_dd_efee_r;
+        }
+        return data.data;
+    });
+    console.log(value);
+    console.log(typeof(value));
+    return value;
+}
+
+exports.nowDate = nowDate();
+exports.apiCrawl = apiCrawl();
--- app.js
+++ app.js
@@ -1,72 +1,21 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
-const axios = requie("axios");
-// const urlCrawl = require("./dataCrawl.json");
+const port = process.env.PORT||5000;
+const {nowDate,apiCrawl} = require("./apicrawl");
 
 app.get("/",function(req,res){
     res.send("Server Set start with Heroku");
 });
 
 app.get("/nowdate",function(req,res){
-    let now = new Date;
-    let year = String(now.getFullYear());
-    let month = function(){
-        let buf = String(now.getMonth()+1);
-        if(buf.length!==2){
-            return "0"+buf;
-        }
-        else{
-            return buf;
-        }
-    };
-    let date =  function(){
-        let buf = String(now.getDate());
-        if(buf.length!==2){
-            return "0"+buf;
-        }
-        else{
-            return buf;
-        }
-    }
-    res.send(year+month()+date());
+    let sendData = nowDate;
+    res.send(sendData);
 });
 
-// app.get("/exchange_rate/nowdate",function(req,res){
-//     let nowDate = function(){
-//         let now = new Date;
-//         let year = String(now.getFullYear());
-//         let month = function(){
-//             let buf = String(now.getMonth()+1);
-//             if(buf.length!==2){
-//                 return "0"+buf;
-//             }
-//             else{
-//                 return buf;
-//             }
-//         };
-//         let date =  function(){
-//             let buf = String(now.getDate());
-//             if(buf.length!==2){
-//                 return "0"+buf;
-//             }
-//             else{
-//                 return buf;
-//             }
-//         }
-//         return (year+month()+date());
-//     }
-//     console.log(nowDate());
-//     axios.get(urlCrawl.url1+urlCrawl.authkey+urlCrawl.url2+nowDate()+urlCrawl.url3).then(function(data){
-//         console.log(data);
-//         for(let i = 0;i<data.data.length;i++){
-//             delete data.data[i].result;
-//             delete data.data[i].yy_efee_r;
-//             delete data.data[i].ten_dd_efee_r;
-//         }
-//         console.log(data.data);
-//         res.send(data.data);
-//     });
-// });
+app.get("/exchange_rate",async function(req,res){
+    let sendData = await apiCrawl;
+    res.send(sendData);
+    console.log(sendData);
+});
 
 app.listen(port);
